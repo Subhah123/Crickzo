2ae95e0be7c3a0e941d72ff863969d99dd397203
Subhah123 <mahatsubhash02@gmail.com>
2025-06-24 22:03:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
+import Navbar from './Navbar';
+
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <img
-        src="/logo.png"
-        alt="Crickzo Logo"
-        style={{
-          height: "60px",
-          width: "60px",
-          borderRadius: "50%",
-          objectFit: "cover"
-        }}
-      />
-      <h1>Crickzo - Live Cricket Scores</h1>
-      <p>Welcome to Crickzo! Your live cricket update source.</p>
-    </div>
+    <>
+      <Navbar />
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <h1>Welcome to Crickzo</h1>
+        <p>Your live cricket update source.</p>
+      </div>
+    </>
   );
 }
 
