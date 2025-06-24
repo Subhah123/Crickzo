7e32a0fd976081bdcdf7e72bc65343cf85943484
Subhah123 <mahatsubhash02@gmail.com>
2025-06-24 22:32:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from './Navbar';
+import MatchCard from './MatchCard';  // 👈 Add this
 
 function App() {
   return (
     <>
       <Navbar />
+      <MatchCard />  {/* 👈 Render Match Card */}
+
       <div style={{ textAlign: "center", marginTop: "20px" }}>
         <h1>Welcome to Crickzo</h1>
         <p>Your live cricket update source.</p>
