f47a3eb9d299cba2a63334a085a407892f553638
Subhah123 <mahatsubhash02@gmail.com>
2025-06-24 18:17:48 +0530

    Create App.js

---
+++ Src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🏏 Crickzo - Live Cricket Scores</h1>
+      <p>Welcome to Crickzo! Your live cricket update source.</p>
+    </div>
+  );
+}
+
+export default App;
