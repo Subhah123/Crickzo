d96ef7b284f4d98c98b1a9b94725ff90cb1057f3
Subhah123 <mahatsubhash02@gmail.com>
2025-06-25 10:36:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,23 @@
 import React from 'react';
-import './Navbar.css'; // optional styling
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import logo from '../logo.png';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <img
-          src="/logo.png"
-          alt="Crickzo"
-          style={{
-            height: "45px",
-            width: "45px",
-            borderRadius: "50%",
-            objectFit: "cover"
-          }}
-        />
-        <span className="brand-name">Crickzo</span>
+        <img src={logo} alt="Crickzo Logo" className="logo" />
+        <span className="title">Crickzo</span>
       </div>
       <div className="navbar-right">
-        <a href="#">Home</a>
-        <a href="#">Live</a>
-        <a href="#">Fixtures</a>
-        <a href="#">News</a>
-        <a href="#">About</a>
+        <Link to="/">Home</Link>
+        <Link to="/fixtures">Fixtures</Link>
+        <Link to="/news">News</Link>
+        <Link to="/about">About</Link>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
-
