5365347d43a0ccf346ae9530f00b4fa5e243233e
Subhah123 <mahatsubhash02@gmail.com>
2025-06-25 08:35:38 +0530

    Create News.js

---
+++ src/News.js
@@ -0,0 +1,11 @@
+// src/News.js
+function News() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>Latest Cricket News</h2>
+      <p>Stay updated with the latest cricket headlines!</p>
+    </div>
+  );
+}
+
+export default News;
