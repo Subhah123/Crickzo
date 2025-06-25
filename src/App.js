076a3f055259bc8ac39242400f82fc6f40f513ab
Subhah123 <mahatsubhash02@gmail.com>
2025-06-25 08:38:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,28 @@ function App() {
   return (
     <>
       <Navbar />
+        import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import MatchCard from './MatchCard';
+import Fixtures from './Fixtures';
+import News from './News';
+import About from './About';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MatchCard />} />
+        <Route path="/fixtures" element={<Fixtures />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
       {matches.map((match, index) => (
         <MatchCard
           key={index}
