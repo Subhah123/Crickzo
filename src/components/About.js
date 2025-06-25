073890785f41ac392a1d2afd5da592cff8b8fb72
Subhah123 <mahatsubhash02@gmail.com>
2025-06-25 09:15:16 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+// src/About.js
+function About() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h2>About Crickzo</h2>
+      <p>Crickzo is your one-stop source for live cricket scores and news.</p>
+    </div>
+  );
+}
+
+export default About;
